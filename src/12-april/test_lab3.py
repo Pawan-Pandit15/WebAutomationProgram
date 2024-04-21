e9d50e4d8d83392b6f55dbd9e73f72d0804366a2
Pawan <Pawanpandit332@gmail.com>
2024-04-21 18:19:52 +0530

    web automation

---
+++ src/12-april/test_lab3.py
@@ -0,0 +1,34 @@
+
+# Selenium 4
+
+
+from selenium import webdriver
+
+import time
+import allure
+
+#----------------------------------------------------------------
+# def test_open_vwologin():
+#     driver = webdriver.Chrome()
+#     driver.get("https://app.vwo.com")
+
+#--------------------------------------------------------------------
+
+# def test_open_vwologin():
+#     driver = webdriver.Chrome() # POST request | Create the Session
+#     driver.get("https://app.vwo.com") # GET Request to URL param
+#     print(driver.title)  # here print browser Title
+#     driver.maximize_window()  # here maximize the browser
+#     time.sleep(5)   # here open browser 5 second
+#     print(driver.page_source)  # here print all HTML code in this program
+#     print(driver.session_id)   # here create unique session id
+#     driver.close()   # this is close only one tab
+#     driver.quit()  # this is close all browser tab ( Whole browser )
+#     assert driver.title == "Login - VWO"
+
+#------------------------------------------------------------------------
+
+
+
+
+
